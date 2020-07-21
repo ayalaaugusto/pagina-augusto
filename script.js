578dac9174c4c2e5f9f71b3a85a66d655f8e9849
augusto <ayala_815@hotmail.com>
2020-07-21 19:37:06 -0300

    v 1.2.1

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 let button = document.getElementById("access");
 let links = document.getElementById("links");
-let access = document.getElementById('access');
 let i = 0;
 
 button.addEventListener("click", function(e){
     e.preventDefault();
     if(i==0){
         links.className = ("links two")
-        access.className = ("access acces-two")
         i=1;
     }else{
         links.classList.remove("two")
         links.className = ("links one")
-        access.className = ("access access-one")
         i=0;
     }
 })
